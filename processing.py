e7184b00265744e4060dbdcd47043fca13b1cbd2
Adithya Vasudev <adithya.gvasudev@gmail.com>
2020-10-16 18:47:16 -0700

    First Round Processing

--- processing.py
+++ processing.py
@@ -15,4 +15,9 @@ for i in range(0, data.shape[0]):
     if not (within(ids, data['meter_id'][i])):
         ids.append(data['meter_id'][i])
 ids.pop(0)
-print(ids)        
+print(ids)
+a_file = open("stations.dat", "w")
+for i in ids:
+    a_file.write(i + "\n")
+
+a_file.close()
