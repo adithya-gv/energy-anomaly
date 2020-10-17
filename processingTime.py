a425f60285ec80968b213bdb973174634f4b05f2
Adithya Vasudev <adithya.gvasudev@gmail.com>
2020-10-16 19:54:50 -0700

    Re-processed by day

--- processingTime.py
+++ processingTime.py
@@ -7,8 +7,16 @@ a_file = open("times.dat", "w")
 
 for i in range (0, data.shape[0]):
     a = str(data['Timestamp'][i])
-    time = int(a[11:13]) * 60 + int(a[14:16]) + int(a[20:22]) * 60
-    timeSet.append(time)
-    a_file.write(str(time) + "\n")
+    dayCount = 0
+    time1 = a[5:7]
+    if(time1 in ["01", "03", "05", "07", "08", "10", "12"]):
+        dayCount = dayCount + 31 * int(time1)
+    elif(time1 in ["02"]):
+        dayCount = dayCount + 28 * int(time1)
+    else:
+        dayCount = dayCount + 30 * int(time1)
+    dayCount = dayCount + int(a[8:10])
+    timeSet.append(dayCount)
+    a_file.write(str(dayCount) + "\n")
 
 a_file.close()
