500558743f4fe48a6e00b705bd9a94fd5aea39ae
Adithya Vasudev <adithya.gvasudev@gmail.com>
2020-10-16 18:27:02 -0700

    Data Preprocessing

--- processing.py
+++ processing.py
@@ -1,7 +1,18 @@
-from sklearn.naive_bayes import GaussianNB
+from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 import matplotlib as plt
 
+def within(id, s):
+    for i in id:
+        if i == s:
+            return True
+    return False
+
 data = pd.read_csv("training-set.csv", sep=";")
-print(data.head())
+ids = ["0"]
+for i in range(0, data.shape[0]):
+    if not (within(ids, data['meter_id'][i])):
+        ids.append(data['meter_id'][i])
+ids.pop(0)
+print(ids)        
